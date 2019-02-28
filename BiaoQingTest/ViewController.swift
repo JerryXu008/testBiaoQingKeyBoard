d885b39020811f61aa5148efcba57357e34e2f51
“JeryyXu008” <573398127@qq.com>
2019-02-28 14:06:22 +0800

    Initial Commit

---
+++ BiaoQingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BiaoQingTest
+//
+//  Created by BruceXu on 2019/2/28.
+//  Copyright © 2019年 BruceXu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
